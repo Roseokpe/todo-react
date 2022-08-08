ac40596cd1987f9ec5682d2df83da3427af206ab
Roseokpe <roseokpe@gmail.com>
2022-08-08 06:54:16 -0700

    added setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// import React from 'react';
+
+// <script type="text/babel">
+//   const element = <h1>Hello from React</h1>; ReactDOM.render(element,
+//   document.getElementById("root"));
+// </script>
+
+// export default App;
