e519a0d83754a017fe26aa442d1590576e49a30a
Roseokpe <roseokpe@gmail.com>
2022-08-10 00:23:21 -0700

    added code as instructed in the react tutotial

--- src/App.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-
-// <script type="text/babel">
-//   const element = <h1>Hello from React</h1>; ReactDOM.render(element,
-//   document.getElementById("root"));
-// </script>
-
-// export default App;
